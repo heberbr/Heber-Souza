09c7b859a916b01903c75997640afe7d0770d6ee
Heber <hsouza1983@gmail.com>
2021-09-10 23:02:09 -0300

    atividade logica

---
+++ semana1/operadores/index.js
@@ -0,0 +1,52 @@
+// ----------------------> Exemplos <---------------------------
+
+console.log('Olá, Banu!')
+
+
+// Exercicio 1
+let suaIdade = Number(prompt("Qual a sua idade?"))
+let idadeMelhorAmigo = Number(prompt("Qual a idade do(a) seu melhor amigo(a)?"))
+let idadeMaior = (suaIdade > idadeMelhorAmigo)
+let diferencaIdade = (suaIdade - idadeMelhorAmigo)
+console.log("Sua idade é maior do a do seu melhor amigo?",idadeMaior)
+console.log("A diferença de idade entre vocês é de:", diferencaIdade, "ano(s).")
+
+// Atividade 2
+
+let numeroPar = Number(prompt("Digite um número par."))
+console.log(numeroPar % 2)
+
+// O resultado é sempre 0 pois qualquer número par dividido por 2 não apresenta sobra.
+// O resultado sempre vai apresentar uma sobra.
+
+// Atividade 3
+
+const idadeAnos = Number (prompt('qual a sua idade'))
+
+let ano = 12
+let mes = 30
+let dia = 24
+
+let idadeMeses = ano * idadeAnos
+let idadeDias = idadeMeses * mes
+let idadeHoeras = idadeDias * dia
+
+console.log("Sua idade:" , idadeAnos)
+console.log("Sua idade em meses:" , idadeMeses)
+console.log("Sua idade em dias:" , idadeDias)
+console.log("Sua idade em horas" , idadeHoras)
+
+// Atividade 4
+const soma = 3 + 4
+
+// Multiplicar 3 por 5 e dividir o resultado por 2
+// cria variavel 3 vezes 85
+// divide a variavel anterior por 2
+
+//const resultadoMultiplicacao = 3 * 5
+//const resultado2 = resultadoMultiplicacao / 2
+
+const resultado2 = 3 * 5 / 2
+
+
+
