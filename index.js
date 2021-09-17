5378c57c02bc40f8c0a3142fa94f58cf83320ac6
heberbr <hsouza1983@gmail.com>
2021-09-16 21:43:31 -0300

    atividade funcoes

---
+++ index.js
@@ -0,0 +1,75 @@
+       // Exercicio
+
+       // a) A função não deve receber nenhum parâmetro e deve imprimir uma mensagem falando algumas informações sobre você, como:
+
+       //"Eu sou caio, tenho 23 anos, moro em São Paulo e sou estudante."
+
+       //Troque o nome, idade, cidade e se é estudante ou não por informações sobre você. Lembrando que a função não possui entradas, apenas imprime essa mensagem.
+
+       function imprimeInfo () {
+           const resposta = (imprimeInfo)
+
+       }
+
+       console.log('Eu sou Heber, tenho 38 anos, Moro em Fortaleza e sou estudante')
+
+       
+       // b)
+
+       // function imprimeDadosPessoais(){
+       //  const NOME = prompt("Digite seu nome")
+       //  const IDADE = Number(prompt("Digite sua idade"))
+       //  const ENDERECO = prompt("Digite sua cidade")
+       //  const PROFISSAO = prompt("Digite sua profissao")
+       //  console.log ('Eu sou ${NOME}, tenho ${IDADE} anos, moro em ${ENDERECO} e sou ${PROFISSAO}.')
+       // }
+       
+       // imprimeDadosPessoais()
+
+       // 2 
+       // a) 
+        function somaNumeros(num1, num2){
+            const soma = num1 + num2
+            //console.log(soma)
+            return soma
+        }
+        let numeroDigito1 = Number(prompt("Digite o primeiro numero"))
+        let numeroDigito2 = Number(prompt("Digite o segundo numero"))
+        //let resultado =somaNumeros(numeroDigito1, numeroDigito2)
+        somaNumeros(numeroDigitao1, numeroDigito2)
+        // console.log('Resultado com o prompt:', resultadoComNumerosPrompt)
+
+
+        // 3 
+        function somarNumeros(numeroUsuario1, numeroUsuario2) {
+
+            const soma = numeroUsuario1 + numeroUsuario2
+            return soma 
+        }
+
+        function subtrairNumeros(numeroUsuario1, numeroUsuario2) {
+
+            const subtracao = numeroUsuario1 - numeroUsuario2
+            return
+        }
+
+        function multiplicarNumeros(numeroUsuario1, numeroUsuario2) {
+
+            const multiplicacao = numeroUsuario1 * numeroUsuario2
+            return multiplicacao
+        }
+
+        function dividirNumeros(numeroUsuario1, numeroUsuario2) {
+
+            const divisao = numeroUsuario1 / numeroUsuario2
+            return divisao
+        }
+
+        const numeroUsuario1 = Number(prompt("Digito o primeiro numero"))
+        const numeroUsuario2 = Number(prompt("Digito o segundo numero"))
+
+        console.log("Numeros inseridos:", numeroUsuario1, "e", numeroUsuario2, "Soma:", somarNumeros(numeroUsuario1, numeroUsuario2), "subtracao:", subtrairNumeros(numeroUsuario1, numeroUsuario2), "multiplicacao:", multiplicarnumeros(numeroUsuario1, numeroUsuario2), "divisao:", dividirNumeros(numeroUsuario1, numeroUsuario2))
+
+
+
+       
