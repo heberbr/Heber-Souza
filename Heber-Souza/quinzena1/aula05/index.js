05bb37f0348800cd2cc8d5b4825a7f765c4b6b67
heberbr <hsouza1983@gmail.com>
2021-09-21 16:35:51 -0300

    atividade objetos camila

---
+++ Heber-Souza/quinzena1/aula05/index.js
@@ -0,0 +1,35 @@
+// Exercicio de escrita de código 
+
+// 1 - Faça um programa que pergunte ao usuário seu nome e seu e-mail. Em seguida imprima na tela a seguinte mensagem:
+// O e-mail emailDoUsuario foi cadastrado com sucesso. Seja bem-vindo(a), nomeDoUsuario!
+
+//const nome = prompt("Qual seu nome?")
+// const email = prompt("Qual seu e-mail?")
+
+//console.log("O e-mail", e-mail, "foi cadastro com sucesso. Seja bem-vindo(a)", nomeDoUsuario)
+
+// 2 Faça um programa que contenha um array com 5 das suas comidas preferidas, armazenado em uma variável. Em seguida, siga os passos:
+
+// a) Imprima na tela o array completo
+// b) Imprima na tela a mensagem "Essas são as minhas comidas preferidas:", seguida por cada uma das comidas, uma embaixo da outra.
+// c) Aqui vai um desafio: pergunte ao usuario uma comida preferida. Troque a segunda comida da sua lista pela inserida pelo usuário. Imprima na tela a nova lista.
+
+//let comida = ["arroz" , "feijao" , "pizza" , "carne" , "batata-frita"]
+//console.log(comida)
+
+// 3 Faca um programa, seguindo os passos:
+// a) Cria um array vazio e guarde-o em uma variável, chamada listaDeTarefas
+// b) Pergunte ao usuario 3 tarefas que ele precise realizar no dia e armazene-as, uma por uma, no array
+// c) Imprima o array na tela
+// d) Peca ao usuario que digite o indice de uma tarefa que ele ja realizou: 0,1 ou 2
+// e) Remova da lista o item de indice que o usuario escolheu
+// f) Imprima o array na Tela
+
+// let listaDeTarefas = [],
+// let tarefa1 = prompt ("Digite sua primeira tarefa a realizar")
+// let tarefa2 = prompt ("Digite sua segunda tarefa a realizar")
+// let tarefa3 = prompt ("Digite sua terceira tarefa a realizar")
+
+// listaDeTarefas.push(tarefa1, tarefa2, tarefa3);
+
+// console.log ("Tarefas do dia:", listaDeTarefas)
---
+++ Heber-Souza/quinzena1/aula07/objetos/index.js
@@ -0,0 +1,65 @@
+// exercicio 1
+
+const pessoa = {
+    nome: "Heber"
+    apelidos: ["Para", "Feijao", "Chocolate"]
+}
+
+function descricao(pessoa) {
+
+}
+
+console.log(`Me chamo ${pessoa.nome}, mas pode me chamar de ${pessoa.apelidos}`)
+
+// exercicio 2
+
+const pessoa1 = {
+    nome1: "Carina"
+    idade1: 38,
+    profissao1: "Administradora"
+}
+
+const pessoa2 = {
+    nome2: "Heber"
+    idade2: 38,
+    profissao2: "Tecnologo"
+}
+
+function lista(pessoa1) {
+    const array = [pessoa1.nome1, pessoas1.nome1.lenght, pessoa1.idade1, pessoa1.profissao1, pessoa1.profissao1.lenght]
+    return array
+}
+console.log(lista(pessoa1))
+
+
+
+
+
+// exercicio 03
+
+console.log("Exercicio 03")
+
+const carrinho = []
+
+const fruta1 = {
+    nome: "Uva"
+    disponibilidade: true
+}
+
+const fruta2 = {
+    nome: "banana"
+    disponibilidade: true
+}
+
+const fruta3 = {
+    nome: "kiwi"
+    disponivel: true
+}
+
+function frutacarrinho(fruta1, fruta2, fruta3) {
+    carrinho.push(fruta1)
+    carrinho.push(fruta2)
+    carrinho.push(fruta3)
+
+    return carrinho
+}
