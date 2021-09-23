5b31b8ab6cff5cb33c10be7dec9f13b260b811ba
heberbr <hsouza1983@gmail.com>
2021-09-23 16:27:20 -0300

    atividade08 condicionais

---
+++ condicionais/index.js
@@ -0,0 +1,70 @@
+//exercicio 1
+
+//var nome = prompt("Digite seu nome")
+//var idade = Number(prompt("Digite sua idade"))
+
+//if(idade >= 18){
+    console.log("Você pode dirigir")
+// }else{
+   // if(idade < 18){
+    //    console.log("Você não pode dirigir")
+    //}
+//}
+
+// exercicio 2
+
+//const turnos = prompt("Bom dia, Boa tarde, Boa noite")
+//if (turnos &&M){
+ //   console.log("Bom dia")
+//} elseif (turnos V) 
+//console.log("Boa Tarde") 
+  //elseif (turnos N)
+ //   console.log("Boa Noite")
+    
+
+
+
+
+
+// exercicio 3
+
+//const turnoAluno = prompt("Turno de estudo do aluno")
+
+//switch (turnoAluno){
+ //   case "M":
+ //       console.log("Bom dia!")
+ //       break
+ //       case "V":
+ //           console.log("Boa tarde!")
+ //           break
+       //     case "V":
+     //           console.log("Boa Noite!")
+   //             break
+ //              console.log("Turno não encontrado. Escolha algum dos turnos possiveis. 'Matutino', 'Vespertino'ou 'Noturno'")
+// }
+   
+// exercicio 4
+
+//const ingressoDozereais = confirm("O ingresso está abaixo de 15 reais?")
+//const filmeGeneroFantasia = confirm("O filme é do genero Fantasia?")
+
+// function filmeNoCinema
+//(ingressoDozereais, 
+ //filmeGeneroFantasia   
+   // ) {
+
+    //if(
+      //  ingressoDozereais && 
+        //filmeGeneroFantasia
+        //){
+        //console.log("Bom filme!")
+
+//    } else {
+  //      console.log("Escolha outro filme!")
+    //}
+//}
+
+//filmeNoCinema (
+  //  ingressoDozereais,
+    //filmeGeneroFantasia
+//)
