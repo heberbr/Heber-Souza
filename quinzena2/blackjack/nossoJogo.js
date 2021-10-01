e9b6be0b4be410b1f04bb6b79ad4a276e5b21c96
heberbr <hsouza1983@gmail.com>
2021-10-01 15:52:24 -0300

    projeto blackjack

--- quinzena2/blackjack/nossoJogo.js
+++ quinzena2/blackjack/nossoJogo.js
@@ -1,6 +1,20 @@
-console.log("Boas vindas ao jogo de blackjack")
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+    console.log("Bem-vindo(a) ao jogo de blackjack")
 
 let jogo = confirm("Quer iniciar uma nova rodada?")
+
 
 
 if (jogo) {
