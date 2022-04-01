a24855ed008c4665032a852b926e77c5fdc0a062
heber <hsouza1983@gmail.com>
2022-03-31 21:18:28 -0300

    atividade express

--- aprofundamento-express-/src/data.ts
+++ aprofundamento-express-/src/data.ts
@@ -40,4 +40,33 @@ export const users = [
         },
      ],
   },
+  {
+   id: 'iago-soriano-joy',
+   playlists: [
+      {
+         id: '28e07ef5-4c6f-4478-9998',
+         name: 'rap',
+         tracks: [
+            {
+               id: '8b4215f1-950b-4c06-bfd6',
+               name: 'Rap',
+               artist: 'Cantor de Rap',
+               url: 'https://www.youtube.com/watch?v=y5rY2N1XuLI',
+            }
+         ],
+      },
+      {
+         id: '4c6f-4478-9998-1fae4c6d4543',
+         name: 'Funk',
+         tracks: [
+            {
+               id: '950b-4c06-bfd6-80c2f8d1cc08',
+               name: 'Funk',
+               artist: 'Cantor de funk',
+               url: 'https://www.youtube.com/watch?v=0dLX40UMUKo',
+            }
+         ],
+      },
+   ],
+},
 ];
